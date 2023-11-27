7e808a8775698a5f6db0938e899a5f8f6abd3967
NFartenadze <nfartenadze@solvd.com>
2023-11-27 13:30:04 +0400

    new tasks

---
+++ task8.js
@@ -0,0 +1,39 @@
+// Create the function prefill that returns an array of n elements that all have the same value v. See if you can do this without using a loop.
+
+// You have to validate input:
+
+//     v can be anything (primitive or otherwise)
+//     if v is omitted, fill the array with undefined
+//     if n is 0, return an empty array
+//     if n is anything other than an integer or integer-formatted string (e.g. '123') that is >=0, throw a TypeError
+
+// When throwing a TypeError, the message should be n is invalid, where you replace n for the actual value passed to the function.
+
+function prefill(num, value) {
+  if (value == "undefined") {
+    return [undefined];
+  }
+  if (num == 0) {
+    return [];
+  }
+  if (num === 0) return [];
+
+  if (!parseInt(num) || num < 0) {
+    let typeError = new TypeError();
+    typeError.name = "TypeError";
+    typeError.message = num + " is invalid";
+    throw typeError;
+  }
+
+  return Array.from({ length: num }, () => value);
+}
+
+console.log(prefill(3, 1)); //--> [1,1,1]
+
+console.log(prefill(2, "abc")); //--> ['abc','abc']
+
+console.log(prefill("1", 1)); // --> [1]
+
+console.log(prefill(3, prefill(2, "2d"))); //--> [['2d','2d'],['2d','2d'],['2d','2d']]
+
+// console.log(prefill("xyz", 1)); // --> throws TypeError with message "xyz is invalid"
